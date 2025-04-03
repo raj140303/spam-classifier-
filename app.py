4c9e61634e98a911e1f03d790475bdd424682702
raj140303 <ayush091003@gmail.com>
2025-04-03 12:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,9 @@
-# from flask import Flask, render_template, request
-# import nltk
-# from nltk.corpus import stopwords
-# from nltk.stem.porter import PorterStemmer
-# import pickle
-# import string
-
-# app = Flask(__name__)
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
-
-# ps = PorterStemmer()
-
-# def transform_text(text):
-#     text = text.lower()
-#     text = nltk.word_tokenize(text)
-
-#     y = []
-#     for i in text:
-#         if i.isalnum():
-#             y.append(i)
-
-#     text = y[:]
-#     y.clear()
-
-#     for i in text:
-#         if i not in stopwords.words('english') and i not in string.punctuation:
-#             y.append(i)
-
-#     text = y[:]
-#     y.clear()
-
-#     for i in text:
-#         y.append(ps.stem(i))
-
-#     return " ".join(y)
-
-# def predict_spam(message):
-
-#     # Preprocess the message 
-#     transformed_sms = transform_text(message)
-#     # Vectorize the preprocessed message 
-#     vector_input = tfidf.transform([transformed_sms])
-#     # Predict using ML model 
-#     result = model.predict(vector_input)[0]
-#     return result
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         input_sms = request.form['message']
-#         result = predict_spam(input_sms)
-#         return render_template('index.html', result=result)  # Pass 'result' to the template
-
-
-# if __name__ == '__main__':
-#     tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-#     model = pickle.load(open('model.pkl', 'rb'))
-#     app.run(host='0.0.0.0')
-# # localhost ip address 0.0.0.0
-
-
-
 from flask import Flask, render_template, request
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import pickle
-import string  
+import string
 
 app = Flask(__name__)
 
@@ -79,9 +11,10 @@ app = Flask(__name__)
 nltk.download('punkt')
 nltk.download('stopwords')
 
+# Initialize the PorterStemmer
 ps = PorterStemmer()
 
-# Load model and vectorizer globally
+# Load the TF-IDF vectorizer and model globally
 try:
     tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
     model = pickle.load(open('model.pkl', 'rb'))
@@ -90,25 +23,42 @@ except Exception as e:
     exit()
 
 def transform_text(text):
+    """
+    Converts the input text to lower case, tokenizes it, removes non-alphanumeric tokens,
+    filters out stopwords and punctuation, and applies stemming.
+    """
     text = text.lower()
-    text = nltk.word_tokenize(text)
-
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
-
-    text = [ps.stem(i) for i in text]
-
-    return " ".join(text)
+    tokens = nltk.word_tokenize(text)
+    
+    # Filter out non-alphanumeric tokens
+    filtered_tokens = [token for token in tokens if token.isalnum()]
+    
+    # Remove stopwords and punctuation
+    cleaned_tokens = [
+        token for token in filtered_tokens
+        if token not in stopwords.words('english') and token not in string.punctuation
+    ]
+    
+    # Apply stemming to each token
+    stemmed_tokens = [ps.stem(token) for token in cleaned_tokens]
+    
+    return " ".join(stemmed_tokens)
 
 def predict_spam(message):
+    """
+    Transforms the input message and predicts whether it is spam using the loaded model.
+    """
     try:
         transformed_sms = transform_text(message)
         vector_input = tfidf.transform([transformed_sms])
         result = model.predict(vector_input)[0]
+        
+        # Debugging output
+        print("DEBUG: Original message:", message)
+        print("DEBUG: Transformed message:", transformed_sms)
+        print("DEBUG: Vector input shape:", vector_input.shape)
+        print("DEBUG: Prediction:", result)
+        
         return result
     except Exception as e:
         print(f"Prediction error: {e}")
